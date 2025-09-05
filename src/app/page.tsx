d77aa8857d5aabd1422d46fe04395239c0621609
vikrant dubey <Vikrant2003aug06@gmail.com>
2025-09-05 22:00:01 +0530

    setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,8 @@
 
 export default function Home() {
   return (
-    <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
-    
+    <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02] text-center text-2xl">
+       Vikrant dubey is here
     </main>
   );
 }
