4b7b4a68215253659c48cf806c3b82c66aedeb31
vikrant dubey <Vikrant2003aug06@gmail.com>
2025-09-06 16:41:06 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,6 @@ import Footer from "@/components/Footer";
 export default function Home() {
   return (
     <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02] text-center text-2xl">
-      <h2>page. tsx</h2>
       <HeroSection/>
       <FeaturedSection/>
      <Testimonial/>
