444901a05658c0a10ea0f9b9cd3c419acc513295
vikrant dubey <Vikrant2003aug06@gmail.com>
2025-09-06 17:01:59 +0530

    update

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,4 @@
-import { Meteors } from "@/components/ui/meteors";
+
 import type { Config } from "tailwindcss";
 
 const svgToDataUri = require("mini-svg-data-uri");
@@ -55,7 +55,7 @@ const config: Config = {
       animation:{
         spotlight: "spotlight 2s ease .75s 1 forwards",
         scroll: "scroll var(--animation-duration, 40s) var(--animation-direction, forwards) linear infinite",
-         "meteor-effect": "meteor 5s linear infinite",
+         
       },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
@@ -70,14 +70,6 @@ const config: Config = {
         scroll: {
           to: {
             transform: "translate(calc(-50% - 0.5rem))",
-          },
-        },
-        meteors: {
-          "0%": { transform: "rotate(215deg) translateX(0)", opacity: "1" },
-          "70%": { opacity: "1" },
-          "100%": {
-            transform: "rotate(215deg) translateX(-500px)",
-            opacity: "0",
           },
         },
 
